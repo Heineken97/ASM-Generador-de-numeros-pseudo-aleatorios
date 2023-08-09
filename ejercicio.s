89af2d88cbf0299daa238633ca35fde7c41b7a4b
Joseph Jimenez <josephdjz@hotmail.com>
2023-08-09 10:22:23 -0600

    Add files via upload

--- ejercicio.s
+++ ejercicio.s
@@ -3,9 +3,9 @@ main:
     li a0, 0b01011010 
     # colocar semilla en la posicion de memoria 0x100
     li a1, 0x100 
-    # Almacenamos la semilla en la direcciÃ³n de memoria
+    # Almacenamos la semilla en la dirección de memoria
     sw a0, 0(a1)    
-    # Contador para obtener los 100 nÃºmeros aleatorios
+    # Contador para obtener los 100 números aleatorios
     li a2, 0x64  
 lfsr_loop:
     # Cargamos LFSR: x^8 +x^6 + x^5 + x^4 + 1
@@ -19,22 +19,18 @@ lfsr_loop:
     xor t6, t5, t3 
     andi t4, t6, 0x1    #Obtener solo el bit resultante del XOR
     # Paso 3.Se hace rotaciones
-    slli t4, t4, 0x7    # Desplazamos el resultado a la izquierda para colocar en el MSB
+    slli t4, t4, 0x8    # Desplazamos el resultado a la izquierda para colocar en el MSB
     srli t5, a0, 0x1    # Desplazamos el resultado a la derecha para descartar LSB
     # Paso 4. Se actualizan los valores del registro
     add a0, t5, t4      # Sumamos dato con MSB y LSB  
 update:   
     # Paso 4.Se actualizan los valores del registro y se realiza de nuevo el paso 1
-    addi a1, a1, 0x4    # Incrementamos la direcciÃ³n para almacenar el siguiente 
-    sw a0, 0(a1)        # Almacenamos el nuevo dato en la posiciÃ³n actual 
-    addi a2, a2, -0x1   # Decrementamos el contador de nÃºmeros aleatorios
-    bnez a2, lfsr_loop  # Saltamos al loop si no hemos alcanzado los 100 nÃºmeros aleatorios
+    addi a1, a1, 0x4    # Incrementamos la dirección para almacenar el siguiente 
+    sw a0, 0(a1)        # Almacenamos el nuevo dato en la posición actual 
+    addi a2, a2, -0x1   # Decrementamos el contador de números aleatorios
+    bnez a2, lfsr_loop  # Saltamos al loop si no hemos alcanzado los 100 números aleatorios
 end:
     # Fin del programa
-
-
-
-
 
 
 
